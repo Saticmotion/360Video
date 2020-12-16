682ddfc065665a7cc43935903a2570e8aed4666b
jitsew <32553997+jitsew@users.noreply.github.com>
2020-12-16 11:24:34 +0100

    Add build settings window

--- Assets/Scripts/Editor/AutomatedBuild.cs
+++ Assets/Scripts/Editor/AutomatedBuild.cs
@@ -1,78 +1,92 @@
 using UnityEditor;
 using System.Diagnostics;
 using System.IO;
-
+using UnityEngine;
 
 public class AutomatedBuild : EditorWindow
 {
 	[MenuItem("Build/Win64 %&b", false, 1)]
 	public static void BuildWin64()
 	{
-		string branch = GetBranch();
-		BuildPlayerOptions options;
+		BuildSettingsWindow buildSettings = new BuildSettingsWindow();
+		buildSettings.Init();
+		buildSettings.Show();
 
-		string path = "builds/" + branch + "/EditorWin/";
-		options = new BuildPlayerOptions 
-		{ 
-			scenes = new[] { "Assets/Editor.unity" }, 
-			locationPathName = path + "VivistaEditor.exe", 
-			target = BuildTarget.StandaloneWindows64, 
-			options = 0 
-		};
-		PlayerSettings.virtualRealitySupported = false;
-		PlayerSettings.fullScreenMode = UnityEngine.FullScreenMode.Windowed;
-		PlayerSettings.defaultIsNativeResolution = true;
-		PlayerSettings.usePlayerLog = true;
-		PlayerSettings.resizableWindow = true;
-		BuildPipeline.BuildPlayer(options);
+		if (buildSettings.success)
+		{
+			string branch = GetBranch();
+			BuildPlayerOptions options;
 
-		path = "builds/" + branch + "/PlayerWin/";
-		options = new BuildPlayerOptions 
-		{ 
-			scenes = new[] { "Assets/Player.unity" }, 
-			locationPathName = path + "VivistaPlayer.exe", 
-			target = BuildTarget.StandaloneWindows64, 
-			options = 0 
-		};
-		PlayerSettings.virtualRealitySupported = true;
-		BuildPipeline.BuildPlayer(options);
+			string path = "builds/" + branch + "/EditorWin/";
+			options = new BuildPlayerOptions
+			{
+				scenes = new[] { "Assets/Editor.unity" },
+				locationPathName = path + "VivistaEditor.exe",
+				target = BuildTarget.StandaloneWindows64,
+				options = 0
+			};
+			PlayerSettings.virtualRealitySupported = false;
+			PlayerSettings.fullScreenMode = UnityEngine.FullScreenMode.Windowed;
+			PlayerSettings.defaultIsNativeResolution = true;
+			PlayerSettings.usePlayerLog = true;
+			PlayerSettings.resizableWindow = true;
+			BuildPipeline.BuildPlayer(options);
 
-		ShowInWindowsExplorer("builds/" + branch);
+			path = "builds/" + branch + "/PlayerWin/";
+			options = new BuildPlayerOptions
+			{
+				scenes = new[] { "Assets/Player.unity" },
+				locationPathName = path + "VivistaPlayer.exe",
+				target = BuildTarget.StandaloneWindows64,
+				options = 0
+			};
+			PlayerSettings.virtualRealitySupported = true;
+			BuildPipeline.BuildPlayer(options);
+
+			ShowInWindowsExplorer("builds/" + branch);
+		}
 	}
 
 	[MenuItem("Build/OSX")]
 	static void BuildOSX()
 	{
-		string branch = GetBranch();
-		BuildPlayerOptions options;
+		BuildSettingsWindow buildSettings = new BuildSettingsWindow();
+		buildSettings.Init();
+		buildSettings.Show();
 
-		string path = "builds/" + branch + "/EditorOSX/";
-		options = new BuildPlayerOptions 
-		{ 
-			scenes = new[] { "Assets/Editor.unity" }, 
-			locationPathName = path + "VivistaEditor.exe", 
-			target = BuildTarget.StandaloneOSX, 
-			options = 0 
-		};
-		PlayerSettings.virtualRealitySupported = false;
-		PlayerSettings.fullScreenMode = UnityEngine.FullScreenMode.Windowed;
-		PlayerSettings.defaultIsNativeResolution = true;
-		PlayerSettings.usePlayerLog = true;
-		PlayerSettings.resizableWindow = true;
-		BuildPipeline.BuildPlayer(options);
+		if (buildSettings.success)
+		{
+			string branch = GetBranch();
+			BuildPlayerOptions options;
 
-		path = "builds/" + branch + "/PlayerOSX/";
-		options = new BuildPlayerOptions 
-		{ 
-			scenes = new[] { "Assets/Player.unity" }, 
-			locationPathName = path + "VivistaPlayer.exe", 
-			target = BuildTarget.StandaloneOSX, 
-			options = 0 
-		};
-		PlayerSettings.virtualRealitySupported = false;
-		BuildPipeline.BuildPlayer(options);
+			string path = "builds/" + branch + "/EditorOSX/";
+			options = new BuildPlayerOptions
+			{
+				scenes = new[] { "Assets/Editor.unity" },
+				locationPathName = path + "VivistaEditor.exe",
+				target = BuildTarget.StandaloneOSX,
+				options = 0
+			};
+			PlayerSettings.virtualRealitySupported = false;
+			PlayerSettings.fullScreenMode = UnityEngine.FullScreenMode.Windowed;
+			PlayerSettings.defaultIsNativeResolution = true;
+			PlayerSettings.usePlayerLog = true;
+			PlayerSettings.resizableWindow = true;
+			BuildPipeline.BuildPlayer(options);
 
-		ShowInWindowsExplorer("builds/" + branch);
+			path = "builds/" + branch + "/PlayerOSX/";
+			options = new BuildPlayerOptions
+			{
+				scenes = new[] { "Assets/Player.unity" },
+				locationPathName = path + "VivistaPlayer.exe",
+				target = BuildTarget.StandaloneOSX,
+				options = 0
+			};
+			PlayerSettings.virtualRealitySupported = false;
+			BuildPipeline.BuildPlayer(options);
+
+			ShowInWindowsExplorer("builds/" + branch);
+		}
 	}
 
 	public static string GetBranch()
@@ -104,5 +118,56 @@ public class AutomatedBuild : EditorWindow
 			}
 		};
 		proc.Start();
+	}
+}
+
+public class BuildSettingsWindow : EditorWindow
+{
+	public bool success = false;
+	
+	private bool official = false;
+	private string oldVersion = "1.0";
+	private string newVersion = "0.0";
+
+	[MenuItem("Example/Simple Recorder")]
+	public void Init()
+	{
+		const int width = 400;
+		const int height = 100;
+
+		var x = (Screen.currentResolution.width - width) / 2;
+		var y = (Screen.currentResolution.height - height) / 3f;
+
+		GetWindow<BuildSettingsWindow>().position = new Rect(x, y, width, height);
+
+		//TODO(Jitse): Get current file version
+	}
+
+	void OnGUI()
+	{
+		official = EditorGUILayout.Toggle("Official build", official);
+		newVersion = EditorGUILayout.TextField("File version", newVersion);
+
+		EditorGUILayout.Space();
+		if (GUILayout.Button("Build"))
+		{
+			if (oldVersion.Equals(newVersion))
+			{
+				EditorGUILayout.LabelField("Please choose a new version.");
+			}
+			else
+			{
+
+			}
+		}
+
+		if (oldVersion.Equals(newVersion))
+		{
+			EditorGUILayout.LabelField("Please choose a new version.");
+		}
+		else
+		{
+			EditorGUILayout.LabelField("");
+		}
 	}
 }
